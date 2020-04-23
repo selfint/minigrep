c8cc1bdf86f77e91f32fa07f78fd98a6b155a30a
Tom Selfin <selfint@gmail.com>
2020-04-23 21:38:23 +0300

    add case insensitive tests, improve implementation

--- src/lib.rs
+++ src/lib.rs
@@ -43,10 +43,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut matching_lines: Vec<&str> = vec![];
-    let case_insensitive_query = &query.to_lowercase();
+    let query = &query.to_lowercase();
 
     for line in contents.lines() {
-        if line.to_lowercase().contains(case_insensitive_query) {
+        if line.to_lowercase().contains(query) {
             matching_lines.push(line);
         }
     }
@@ -93,13 +93,29 @@ mod tests {
     }
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
