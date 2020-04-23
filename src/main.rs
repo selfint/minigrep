5385a9452742e12ae61d15ccf0e4833a0c7fe8e1
Tom Selfin <selfint@gmail.com>
2020-04-23 21:23:53 +0300

    format unused variables as _

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(&args).unwrap_or_else(|_| {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(_) = minigrep::run(config) {
         process::exit(1);
     }
 }
